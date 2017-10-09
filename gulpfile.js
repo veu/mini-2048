f66be7f24a116098dc7b7c804677d550d77dbb9b
Becky <becky@monometric.net>
2017-10-09 20:16:43 +0200

    494 gist version

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const bom = require('gulp-bom');
+const fs = require('fs');
+const gulp = require('gulp');
+const replace = require('gulp-replace');
+const size = require('gulp-size');
+
+gulp.task('default', ['compile'], function () {
+    gulp.watch('src/*', ['compile']);
+});
+
+gulp.task('compile', function () {
+  const js = fs.readFileSync('src/2048.js', 'utf8');
+  const jsCompiled = js.trim().replace(/(?:\/\/[^\n]*)?\s*\n\s*/g, '');
+  return gulp.src('src/2048.html')
+    .pipe(replace(/\s*\n\s*/g, ''))
+    .pipe(replace(/<demo>/, jsCompiled))
+    .pipe(bom())
+    .pipe(size({pretty: false, showFiles: true}))
+    .pipe(gulp.dest('dist'));
+});
